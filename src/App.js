f264dddae66afd5f470a36ce12d1a0e70c674ff3
Bilguun <n.bilguun@gmail.com>
2020-11-06 16:37:43 +0800

    bolson

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Сайн уу?Билгүүн
         </a>
       </header>
     </div>
